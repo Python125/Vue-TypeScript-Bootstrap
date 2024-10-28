16077e7b564e2d4be9715fbcc452717ec9f72a0c
PythonMonty <andrewnovosel90@gmail.com>
2024-10-28 18:03:29 -0400

    Currently attempting to create dropdown menu for utility navbar

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -1,18 +1,29 @@
 <template>
-    <nav class="utility">
-        <div class="dropdown">
-            <button class="btn dropdown-toggle"><span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                <li><a href="#">Universal Orlando Resort</a></li>
-                <li><a href="#">Universal Studios Hollywood, CA</a></li>
-                <li><a href="#">Universal Studios Japan</a></li>
-                <li><a href="#">Universal Beijing Resort</a></li>
-                <li><a href="#">Universal Studios Singapore</a></li>
-            </ul>
-        </div>
-    </nav>
-    <nav class="main-links"></nav>
-    <nav class="quick-bar"></nav>
+  <nav class="utility navbar navbar-default">
+    <div class="dropdown" @click="toggleDropdown">
+      <button class="btn dropdown-toggle" type="button"><span class="caret"></span></button>
+      <ul class="dropdown-menu">
+        <li><a href="#">Universal Orlando Resort</a></li>
+        <li><a href="#">Universal Studios Hollywood, CA</a></li>
+        <li><a href="#">Universal Studios Japan</a></li>
+        <li><a href="#">Universal Beijing Resort</a></li>
+        <li><a href="#">Universal Studios Singapore</a></li>
+      </ul>
+    </div>
+
+    <!-- <div class="dropdown">
+      <button class="btn dropdown-toggle">Help<span class="caret"></span></button>
+      <ul class="dropdown-menu">
+        <li><a href="#">Universal Orlando Resort</a></li>
+        <li><a href="#">Universal Studios Hollywood, CA</a></li>
+        <li><a href="#">Universal Studios Japan</a></li>
+        <li><a href="#">Universal Beijing Resort</a></li>
+        <li><a href="#">Universal Studios Singapore</a></li>
+      </ul>
+    </div> -->
+  </nav>
+  <nav class="main-links"></nav>
+  <nav class="quick-bar"></nav>
 </template>
 
 <script></script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).use(router).mount("#app");
