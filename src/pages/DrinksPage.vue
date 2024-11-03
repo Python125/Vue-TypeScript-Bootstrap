e99a55bfd9bf39fcf2ff71dd6ffad07550040c62
PythonMonty <andrewnovosel90@gmail.com>
2024-11-03 11:52:13 -0500

    Fixing the drinks page

--- src/pages/DrinksPage.vue
+++ src/pages/DrinksPage.vue
@@ -1,50 +1,43 @@
 <template>
   <h1 class="restaurant text-white text-center">Leaky Cauldron™</h1>
   <div class="menu-items text-white row py-5">
-
-    <div class="col text-center">
-      <div class="beverages-items row">
-        <h3 class="fw-bold text-center">Breakfast Beverages</h3>
-        <div class="col text-center">
-          <p class="fw-bold">Pumpkin Juice™ - $5.49</p>
-          <p class="fw-bold">Fresh Brewed Coffee - $3.79</p>
-          <p class="fw-bold">Apple Juice - $3.49</p>
-        </div>
-        <div class="col text-center">
-          <p class="fw-bold">Orange Juice - $3.49</p>
-        </div>
-      </div>
-    </div>
-
     <div class="beverages-items row">
-      <h3 class="fw-bold text-center">Lunch and Dinner Beverages</h3>
-      <p class="p-5">Butterbeer™ is suitable for those with gluten, soy, egg, wheat, and nut allergies. The topping does contain trace amounts of dairy, so it is unsuitable for vegans or anyone with a dairy allergy. Please ensure to select the Non-Dairy option when selecting your cold or frozen Butterbeer™. Hot Butterbeer™ will remain unsuitable for vegans or anyone with a dairy allergy.</p>
+      <p class="butterbeer-note text-center">
+          Butterbeer™ is suitable for those with gluten, soy, egg, wheat, and
+          nut allergies. The topping does contain trace amounts of dairy, so it
+          is unsuitable for vegans or anyone with a dairy allergy. Please ensure
+          to select the Non-Dairy option when selecting your cold or frozen
+          Butterbeer™. Hot Butterbeer™ will remain unsuitable for vegans or
+          anyone with a dairy allergy.
+        </p>
       <div class="col text-center">
-        <p class="fw-bold">Butterbeer™ Souvenirs</p>
-        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
         <!-- <img :src="butterbeer" alt="Butterbeer"> -->
         <p class="fw-bold">Butterbeer™ - $8.99</p>
         <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch</p>
         <p class="fw-bold">Frozen Butterbeer™ - $8.99</p>
         <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch</p>
+        <p class="fw-bold">Hot Butterbeer™ - $8.99</p>
+        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch</p>
+        <p class="fw-bold">Butterbeer™ Souvenirs</p>
+        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
+        <p class="fw-bold">Stainless Steel Butterbeer™ Souvenirs</p>
+        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
+        <p class="fw-bold">Frozen Butterbeer™ Souvenirs</p>
+        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
         <p class="fw-bold">Pumpkin Juice™ - $5.49</p>
         <p class="fw-bold">Tongue Tying Lemon Squash - $6.49</p>
         <p>Sweet and tart lemon flavored beverage served with squashed lemon</p>
         <p class="fw-bold">Fishy Green Ale - $6.49</p>
         <p>Smooth and creamy with notes of mint and cinnamon and popping blueberry fish eggs</p>
+      </div>
+
+      <div class="col text-center">
         <p class="fw-bold">Fountain Beverages - $4.49</p>
         <p>Iced Tea, Lemonade, Cider</p>
         <p class="fw-bold">2% Milk - $2.99</p>
         <p class="fw-bold">Juice - $3.49</p>
         <p>orange, apple</p>
-      </div>
-      <div class="col text-center">
-        <p class="fw-bold">Stainless Steel Butterbeer™ Souvenirs</p>
-        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
-        <p class="fw-bold">Frozen Butterbeer™ Souvenirs</p>
-        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch, served in a souvenir cup</p>
-        <p class="fw-bold">Hot Butterbeer™ - $8.99</p>
-        <p>A non-alcoholic sweet drink reminiscent of shortbread and butterscotch</p>
+
         <p class="fw-bold">Gillywater - $6.00</p>
         <p class="fw-bold">Otter's Fizzy Orange Juice - $6.49</p>
         <p>Effervescent fresh orange beverage with notes of vanilla and cinnamon</p>
@@ -55,21 +48,28 @@
         <p class="fw-bold">Chocolate Milk - $2.99</p>
         <p class="fw-bold">Courtesy Cup of Water - $0</p>
       </div>
-    </div>
-
-    <div class="specialty-beer-items row">
+      <div class="specialty-beer-items row">
       <h3 class="fw-bold text-center">Specialty Draught Beer</h3>
       <div class="col text-center">
         <p class="fw-bold">Dragon's Scale - $13.00</p>
         <p>unique lager with a light crisp, malty finish</p>
         <p class="fw-bold">Daisyroot Ale - $13.00</p>
-        <p>refreshing traditional English Mild with a light malty flavor profile and a clean finish</p>
+        <p>
+          refreshing traditional English Mild with a light malty flavor profile
+          and a clean finish
+        </p>
       </div>
       <div class="col text-center">
         <p class="fw-bold">Wizard's Brew - $13.00</p>
-        <p>full bodied, sweet stout with a smooth, light sweetness and clean, dry finish</p>
+        <p>
+          full bodied, sweet stout with a smooth, light sweetness and clean, dry
+          finish
+        </p>
         <p class="fw-bold">Dark Forest Ale - $13.00</p>
-        <p>traditional brown ale with roasty caramel/toffee notes and warming finish</p>
+        <p>
+          traditional brown ale with roasty caramel/toffee notes and warming
+          finish
+        </p>
       </div>
     </div>
 
@@ -97,8 +97,7 @@
         <p>Fire Whisky, Fire and Strongbow Cocktail</p>
       </div>
     </div>
-
-
+    </div>
   </div>
 
   <div class="">
@@ -137,5 +136,10 @@ export default /* class HomePage extends Vue */ {
   background-color: #0a0a0a;
   opacity: 0.6;
   z-index: -1;
+}
+
+.butterbeer-note {
+  padding-right: 55rem;
+  padding-left: 55rem;
 }
 </style>
