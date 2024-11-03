31bd0961e78d3cb346b493a4199c8e8dabd7cd33
PythonMonty <andrewnovosel90@gmail.com>
2024-11-03 11:56:08 -0500

    Fixing the drinks page

--- src/pages/DrinksPage.vue
+++ src/pages/DrinksPage.vue
@@ -3,13 +3,13 @@
   <div class="menu-items text-white row py-5">
     <div class="beverages-items row">
       <p class="butterbeer-note text-center">
-          Butterbeer™ is suitable for those with gluten, soy, egg, wheat, and
-          nut allergies. The topping does contain trace amounts of dairy, so it
-          is unsuitable for vegans or anyone with a dairy allergy. Please ensure
-          to select the Non-Dairy option when selecting your cold or frozen
-          Butterbeer™. Hot Butterbeer™ will remain unsuitable for vegans or
-          anyone with a dairy allergy.
-        </p>
+        Butterbeer™ is suitable for those with gluten, soy, egg, wheat, and nut
+        allergies. The topping does contain trace amounts of dairy, so it is
+        unsuitable for vegans or anyone with a dairy allergy. Please ensure to
+        select the Non-Dairy option when selecting your cold or frozen
+        Butterbeer™. Hot Butterbeer™ will remain unsuitable for vegans or anyone
+        with a dairy allergy.
+      </p>
       <div class="col text-center">
         <!-- <img :src="butterbeer" alt="Butterbeer"> -->
         <p class="fw-bold">Butterbeer™ - $8.99</p>
@@ -37,7 +37,6 @@
         <p class="fw-bold">2% Milk - $2.99</p>
         <p class="fw-bold">Juice - $3.49</p>
         <p>orange, apple</p>
-
         <p class="fw-bold">Gillywater - $6.00</p>
         <p class="fw-bold">Otter's Fizzy Orange Juice - $6.49</p>
         <p>Effervescent fresh orange beverage with notes of vanilla and cinnamon</p>
@@ -49,54 +48,45 @@
         <p class="fw-bold">Courtesy Cup of Water - $0</p>
       </div>
       <div class="specialty-beer-items row">
-      <h3 class="fw-bold text-center">Specialty Draught Beer</h3>
-      <div class="col text-center">
-        <p class="fw-bold">Dragon's Scale - $13.00</p>
-        <p>unique lager with a light crisp, malty finish</p>
-        <p class="fw-bold">Daisyroot Ale - $13.00</p>
-        <p>
-          refreshing traditional English Mild with a light malty flavor profile
-          and a clean finish
-        </p>
+        <h3 class="fw-bold text-center">Specialty Draught Beer</h3>
+        <div class="col text-center">
+          <p class="fw-bold">Dragon's Scale - $13.00</p>
+          <p>unique lager with a light crisp, malty finish</p>
+          <p class="fw-bold">Daisyroot Ale - $13.00</p>
+          <p>refreshing traditional English Mild with a light malty flavor profile and a clean finish</p>
+        </div>
+        <div class="col text-center">
+          <p class="fw-bold">Wizard's Brew - $13.00</p>
+          <p>full bodied, sweet stout with a smooth, light sweetness and clean, dry finish</p>
+          <p class="fw-bold">Dark Forest Ale - $13.00</p>
+          <p>traditional brown ale with roasty caramel/toffee notes and warming finish</p>
+        </div>
       </div>
-      <div class="col text-center">
-        <p class="fw-bold">Wizard's Brew - $13.00</p>
-        <p>
-          full bodied, sweet stout with a smooth, light sweetness and clean, dry
-          finish
-        </p>
-        <p class="fw-bold">Dark Forest Ale - $13.00</p>
-        <p>
-          traditional brown ale with roasty caramel/toffee notes and warming
-          finish
-        </p>
-      </div>
-    </div>
 
-    <div class="draught-beer-item row">
-      <h3 class="fw-bold text-center">Draught Beer</h3>
-      <div class="col text-start">
-        <p class="fw-bold">Draught Beer - $11.25</p>
-        <p>Heineken, Stella Artois, Strongbow, Yuengling</p>
+      <div class="draught-beer-item row">
+        <h3 class="fw-bold text-center">Draught Beer</h3>
+        <div class="col text-start">
+          <p class="fw-bold">Draught Beer - $11.25</p>
+          <p>Heineken, Stella Artois, Strongbow, Yuengling</p>
+        </div>
       </div>
-    </div>
 
-    <div class="wine-items row">
-      <h3 class="fw-bold text-center">Wine by the Glass</h3>
-      <div class="col text-start">
-        <p class="fw-bold py-3">Wine by the Glass - $11.00</p>
-        <p>Chardonnay</p>
-        <p>Cabernet Sauvignon</p>
+      <div class="wine-items row">
+        <h3 class="fw-bold text-center">Wine by the Glass</h3>
+        <div class="col text-start">
+          <p class="fw-bold py-3">Wine by the Glass - $11.00</p>
+          <p>Chardonnay</p>
+          <p>Cabernet Sauvignon</p>
+        </div>
       </div>
-    </div>
 
-    <div class="whisky-item row">
-      <h3 class="fw-bold text-center py-3">Whisky</h3>
-      <div class="col text-start">
-        <p class="fw-bold">Fire Whisky - $12.50</p>
-        <p>Fire Whisky, Fire and Strongbow Cocktail</p>
+      <div class="whisky-item row">
+        <h3 class="fw-bold text-center py-3">Whisky</h3>
+        <div class="col text-start">
+          <p class="fw-bold">Fire Whisky - $12.50</p>
+          <p>Fire Whisky, Fire and Strongbow Cocktail</p>
+        </div>
       </div>
-    </div>
     </div>
   </div>
 
