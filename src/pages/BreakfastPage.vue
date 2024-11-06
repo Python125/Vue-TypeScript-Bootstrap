46aa65ef821acd15147a9381003ffbf11b9e8b6a
PythonMonty <andrewnovosel90@gmail.com>
2024-11-06 12:25:36 -0500

    Reformatting breakfast page (in progress) 2

--- src/pages/BreakfastPage.vue
+++ src/pages/BreakfastPage.vue
@@ -7,34 +7,36 @@
   <!-- <p class="mt-3 h4 text-center text-white"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
   <p class="fw-light text-center text-white p-3">Includes small beverage</p> -->
 
-  <div class="menu-items1 text-white row py-5">
-    <div class="col text-center">
-      <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
-      <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
-      <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
+  <div class="menu-row">
+    <div class="menu-items1 text-white row py-5">
+      <div class="col text-center">
+        <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
+        <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
+        <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
+      </div>
+    </div>
+    <div class="menu-items2 text-white row py-5">
+      <div class="col text-center">
+        <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
+        <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
+        <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
+      </div>
     </div>
   </div>
 
-  <div class="menu-items2 text-white row py-5">
-    <div class="col text-center">
-      <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
-      <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
-      <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
+  <div class="menu-row">
+    <div class="menu-items3 text-white row py-5">
+      <div class="sides col text-center">
+        <p class="fw-bold py-1">Bacon - $2.49</p>
+        <p class="fw-bold py-1">English Bacon - $5.49</p>
+        <p class="fw-bold py-1">Roasted Potatoes - $4.99</p>
+      </div>
     </div>
-  </div>
-
-  <div class="menu-items3 text-white row py-5">
-    <div class="sides col text-center">
-      <p class="fw-bold py-1">Bacon - $2.49</p>
-      <p class="fw-bold py-1">English Bacon - $5.49</p>
-      <p class="fw-bold py-1">Roasted Potatoes - $4.99</p>
-    </div>
-  </div>
-  
-  <div class="menu-items4 text-white row py-5">
-    <div class="sides col text-center">
-      <p class="fw-bold py-1">Blood Sausage - $5.49</p>
-      <p class="fw-bold py-1">Scrambled Eggs - $2.99</p>
+    <div class="menu-items4 text-white row py-5">
+      <div class="sides col text-center">
+        <p class="fw-bold py-1">Blood Sausage - $5.49</p>
+        <p class="fw-bold py-1">Scrambled Eggs - $2.99</p>
+      </div>
     </div>
   </div>
 
@@ -88,47 +90,27 @@ export default /* class HomeView extends Vue */ {};
   padding: 0.5rem;
 }
 
-.menu-items1 {
-  width: 50rem;
-  float: left;
-  padding: 10rem;
-  margin-top: 2rem;
-  margin-left: 20rem;
-  background-color: #0a0a0a;
-  opacity: 0.7;
-  z-index: -1;
+.menu-row {
+  display: flex;
+  justify-content: space-between;
+  margin: -5rem 0;
 }
 
-.menu-items2 {
-  width: 50rem;
-  float: right;
-  padding: 10rem;
-  margin-top: 2rem;
-  margin-right: 20rem;
-  background-color: #0a0a0a;
-  opacity: 0.7;
-  z-index: -1;
-}
-
-.menu-items3 {
-  width: 50rem;
-  float: left;
+.menu-items1, .menu-items2,.menu-items3, .menu-items4 {
+  width: 40%;
+  margin: 10rem;
   padding: 5rem;
-  margin-top: 2rem;
-  margin-left: 40rem;
   background-color: #0a0a0a;
   opacity: 0.7;
   z-index: -1;
 }
 
-.menu-items4 {
-  width: 50rem;
-  float: right;
-  padding: 10rem;
-  margin-top: 2rem;
-  margin-right: 20rem;
-  background-color: #0a0a0a;
-  opacity: 0.7;
-  z-index: -1;
-}
+// .menu-items1 {
+//   margin-left: 20rem;
+// }
+
+// .menu-items2 {
+//   margin-top: 2rem;
+//   margin-right: 20rem;
+// }
 </style>
