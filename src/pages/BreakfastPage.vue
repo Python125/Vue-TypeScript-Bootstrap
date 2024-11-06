940d4f27e233ad1c2d33945a30efbcbf9f21704d
PythonMonty <andrewnovosel90@gmail.com>
2024-11-06 18:20:56 -0500

    Page with 4 menu classes

--- src/pages/BreakfastPage.vue
+++ src/pages/BreakfastPage.vue
@@ -3,40 +3,50 @@
     Leaky Cauldron™
   </h1>
 
-  <div class="menu-items text-white row py-5">
-    <h3 class="fw-bold text-center">Breakfast - served until 10:30 am daily</h3>
-    <p class="mt-3 h4 text-center"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
+  <h3 class="fw-bold text-center text-white mt-5">Breakfast - served until 10:30 am daily</h3>
+  <p class="mt-3 h4 text-center text-white"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
+  <p class="fw-light text-center text-white p-3">Includes small beverage</p>
 
-    <p class="fw-light text-center p-5">Includes small beverage</p>
-    
-    <div class="col text-center">
-      <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
-      <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
-      <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
-    </div>
-
-    <div class="col text-center">
-      <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
-      <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
-      <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
-    </div>
-
-    <div class="side-items row mt-5">
-      <h3 class="fw-bold text-center">Sides</h3>
+  <div class="menu-row1">
+    <div class="menu-items1 text-white row py-5">
       <div class="col text-center">
+        <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
+        <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
+        <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
+      </div>
+    </div>
+    <div class="menu-items2 text-white row py-5">
+      <div class="col text-center">
+        <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
+        <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
+        <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
+      </div>
+    </div>
+  </div>
+
+  <h3 class="fw-bold text-white text-center mb-4">Sides</h3>
+  <div class="menu-row2">
+    <div class="menu-items3 text-white row py-5">
+      <div class="sides col text-center">
         <p class="fw-bold py-1">Bacon - $2.49</p>
         <p class="fw-bold py-1">English Bacon - $5.49</p>
         <p class="fw-bold py-1">Roasted Potatoes - $4.99</p>
       </div>
-      <div class="col text-center">
+      <div class="sides col text-center">
         <p class="fw-bold py-1">Blood Sausage - $5.49</p>
         <p class="fw-bold py-1">Scrambled Eggs - $2.99</p>
       </div>
     </div>
+    <!-- <div class="menu-items4 text-white row py-5">
+      <div class="sides col text-center">
+        <p class="fw-bold py-1">Blood Sausage - $5.49</p>
+        <p class="fw-bold py-1">Scrambled Eggs - $2.99</p>
+      </div>
+    </div> -->
+  </div>
 
-
-    <div class="ms-5">
-      <div class="d-flex px-3 pt-5 mt-5">
+  <div class="ms-5 text-white">
+    <div class="d-flex px-3">
       <p class="px-2"><span class="fw-bolder">GS</span> - Gluten Sensitive</p>
       <p class="px-2"><span class="fw-bolder">DS</span> - Dairy Sensitive</p>
       <p class="px-2"><span class="fw-bolder">ES</span> - Egg Sensitive</p>
@@ -49,11 +59,13 @@
       <p class="px-2"><span class="fw-bolder">SES</span> - Sesame Sensitive</p>
       <p class="px-2"><span class="fw-bolder">SS</span> - Soy Protein Sensitive</p>
     </div>
-    </div>
-    <p class="px-4 py-3 ms-5">Menu items and pricing are subject to change without notice</p>
   </div>
 
-  <div class="">
+  <p class="px-4 py-3 ms-5 text-white">
+    Menu items and pricing are subject to change without notice
+  </p>
+
+  <div>
     <router-link to="/">
       <button>Back to home</button>
     </router-link>
@@ -66,7 +78,7 @@
 // @Options({
 //   components: {},
 // })
-export default /* class HomeView extends Vue */ {}
+export default /* class HomeView extends Vue */ {};
 </script>
 
 <style lang="scss" scoped>
@@ -81,11 +93,34 @@ export default /* class HomeView extends Vue */ {}
   padding: 0.5rem;
 }
 
-.menu-items {
-  padding: 10rem;
-  margin-top: 5rem;
+.menu-row1 {
+  display: flex;
+  justify-content: space-between;
+  margin-top: -rem;
+}
+
+.menu-row2 {
+  display: flex;
+  justify-content: space-between;
+  margin-top: -2rem;
+}
+
+.menu-items1, .menu-items2 {
+  width: 40%;
+  margin: 10rem;
+  padding: 4rem;
   background-color: #0a0a0a;
-  opacity: 0.7;
+  opacity: 0.8;
+  z-index: -1;
+}
+
+.menu-items3, .menu-items4 {
+  width: 100%;
+  margin: 10rem;
+  margin-top: 1rem;
+  padding: 5rem;
+  background-color: #0a0a0a;
+  opacity: 0.8;
   z-index: -1;
 }
 </style>
