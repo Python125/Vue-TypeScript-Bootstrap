a518f7d8568332fa1437b57e0101f32f83ffb188
PythonMonty <andrewnovosel90@gmail.com>
2024-11-07 09:35:31 -0500

    Reformatting Lunch and Dinner page

--- src/pages/LunchDinnerPage.vue
+++ src/pages/LunchDinnerPage.vue
@@ -1,107 +1,36 @@
 <template>
-  <h1 class="restaurant text-white text-center">
-    Leaky Cauldron™
-  </h1>
-  <div class="menu-items text-white row py-5">
-    <h2 class="menu-title fw-bold text-center">Lunch and Dinner Menu</h2>
-    <p class="p-5 h4 text-center">Allergen Note: Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
-    
-    <h3 class="fw-bold text-center">Entrées</h3>
+  <h1 class="restaurant text-white text-center">Leaky Cauldron™</h1>
+  <!-- <h2 class="menu-title fw-bold text-center">Lunch and Dinner Menu</h2>
+  <p class="p-5 h4 text-center">Allergen Note: Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p> -->
 
-    <div class="col text-center">
-      <p class="fw-bold">Beef, Lamb & Guinness Stew (DS, ES, FS, PS, SES, SFS, SS, TNS) - $17.99</p>
-      <p>Served in a warm, crusty bread bowl</p>
-      <p class="fw-bold py-2">Cottage Pie (ES, FS, PS, SES, SFS, TNS) - $17.99</p>
-      <p>Savory combination of beef and chunky vegetables in a potato crust served with a garden salad</p>
-      <p class="fw-bold py-2">Bangers & Mash (ES, FS, PS, SES, SFS, TNS) - $16.99</p>
-      <p>Roasted English sausage, creamy mashed potatoes, roasted tomatoes, sautéed onions and cabbage, minted peas & onion gravy</p>
-      <p>Request no sauce for soy protein-sensitive options.</p>
-      <p class="fw-bold py-2">Mini Pie Combination (ES, PS, SES, TNS) - $18.99</p>
-      <p>Mini cottage pie and mini fisherman's pie served with garden salad</p>
-      <p class="fw-bold py-2">Vegan Curry (DS, ES, FS, PS, SFS, VG) - $17.99</p>
-      <p>Plant based Chick`n and potato curry with pita and served with garden salad</p>
-      <p class="fw-bold py-2">Shepherd's Pasty Pie (DS, ES, FS, SES, SFS, VG) - $17.99</p>
-      <p>Meatless beef crumbles and vegetable stew hand pie; served with creamy stone ground mustard dipping sauce, apple beet salad, and chips</p>
-    </div>
-    <div class="col text-center">
-      <p class="fw-bold">Hunter's Chicken (ES, FS, PS, SES, SFS, TNS) - $16.99</p>
-      <p>Bacon wrapped chicken breast with BBQ gravy, Monterey & Cheddar cheese blend, English minted peas, pea tendrils and chips</p>
-      <p class="fw-bold py-2">Fish & Chips (PS, SFS, TNS) - $17.99</p>
-      <p>Fresh north Atlantic cod battered and fried with chips and tartar sauce</p>
-      <p>(Request no tartar sauce for egg-sensitive options.)</p>
-      <p class="fw-bold py-2">Fisherman's Pie (ES, GS, PS, SES, SS, TNS) - $18.99</p>
-      <p>Salmon, shrimp and cod baked together under a potato crust served with garden salad</p>
-      <p class="fw-bold py-2">Scotch Eggs (PS, SFS, TNS) - $13.99</p>
-      <p>Hard-boiled eggs wrapped in pork sausage, dusted with bread crumbs and fried. Served warm with apple beet salad and mustard sauce</p>
-      <p class="fw-bold py-2">Soup & Salad (ES, FS, PS, SES, SFS, TNS) - $12.99</p>
-      <p>Split Pea & Ham soup served with side garden salad</p>
-      <p class="fw-bold py-2">Ploughman's (PS, SFS, TNS) - $23.99</p>
-      <p>Serves 2</p>
-      <p>A feast of English cheese, crusty bread, field green salad, roasted tomatoes, cornichon pickles, apple beet salad, Branston pickle & scotch eggs</p>
-      <p>(Request no scotch egg for egg-sensitive options.)</p>
-      <p>(Request no scotch egg sauce for fish-sensitive options.)</p>
-    </div>
-
-    <div class="side-items row">
-      <h2 class="fw-bold text-center">Sides</h2>
+  <h3 class="fw-bold text-center">Entrées</h3>
+  <div class="menu-row1">
+    <div class="menu-items1 text-white row py-4">
       <div class="col text-center">
-        <p class="fw-bold">Chips (DS, ES, FS, GS, PS, SES, SFS, SS, TNS, VG) - $4.99</p>
-        <p class="fw-bold">Mashed Potatoes with Gravy (ES, FS, PS, SES, SFS, TNS) - $3.99</p>
-        <p class="fw-bold">Side Garden Salad (DS, ES, FS, GS, PS, SES, SFS, SS, TNS, VG) - $6.99</p>
-      </div>
-      <div class="col text-center">
-        <p class="fw-bold">Scotch Egg (PS, SFS, TNS) - $5.49</p>
-        <p class="fw-bold">Add Banger (1 each) (DS, ES, FS, PS, SES, SFS, TNS) - $5.49</p>
+        <p><b>Beef, Lamb & Guinness Stew (DS, ES, FS, PS, SES, SFS, SS, TNS) - $17.99</b> - Served in a warm, crusty bread bowl</p>
+        <p class="py-2"><b>Cottage Pie (ES, FS, PS, SES, SFS, TNS) - $17.99</b> - Savory combination of beef and chunky vegetables in a potato crust served with a garden salad</p>
+        <p class="py-2"><b>Bangers & Mash (ES, FS, PS, SES, SFS, TNS) - $16.99</b> - Roasted English sausage, creamy mashed potatoes, roasted tomatoes, sautéed onions and cabbage, minted peas & onion gravy (Request no sauce for soy protein-sensitive options.)</p>
+        <p class="py-2"><b>Mini Pie Combination (ES, PS, SES, TNS) - $18.99</b> - Mini cottage pie and mini fisherman's pie served with garden salad</p>
+        <p class="py-2"><b>Vegan Curry (DS, ES, FS, PS, SFS, VG) - $17.99</b> - Plant based Chick`n and potato curry with pita and served with garden salad</p>
+        <p class="py-2"><b>Shepherd's Pasty Pie (DS, ES, FS, SES, SFS, VG) - $17.99</b> - Meatless beef crumbles and vegetable stew hand pie; served with creamy stone ground mustard dipping sauce, apple beet salad, and chips</p>
       </div>
     </div>
-
-    <div class="sandwich-items row">
-      <h2 class="fw-bold text-center">Sandwiches</h2>
-      <p class="text-center">Served with chips</p>
-      <p class="text-center">Gluten-free buns are available upon request.</p>
-      <p></p>
+    <div class="menu-items2 text-white row py-4">
       <div class="col text-center">
-        <p class="fw-bold">Banger (DS, FS, PS, SES, SFS, SS, TNS) - $14.99</p>
-        <p>Roasted English sausage, mustard aioli, roasted tomatoes, sautéed cabbage and onions on a crusted baguette</p>
-        <p>(Request no mustard aioli for egg-sensitive options.)</p>
-      </div>
-      <div class="col text-center">
-        <p class="fw-bold">Specialty Chicken (FS, PS, SES, SFS, SS, TNS) - $15.99</p>
-        <p>Grilled chicken breast, apple butter mayo, Colby cheese, smoky apple bacon, roasted tomatoes on a housemade specialty bun</p>
+        <p><b>Hunter's Chicken (ES, FS, PS, SES, SFS, TNS) - $16.99</b> - Bacon wrapped chicken breast with BBQ gravy, Monterey & Cheddar cheese blend, English minted peas, pea tendrils and chips</p>
+        <p class="py-2"><b>Fish & Chips (PS, SFS, TNS) - $17.99</b> - Fresh north Atlantic cod battered and fried with chips and tartar sauce (Request no tartar sauce for egg-sensitive options.)</p>
+        <p class="py-2"><b>Fisherman's Pie (ES, GS, PS, SES, SS, TNS) - $18.99</b> - Salmon, shrimp and cod baked together under a potato crust served with garden salad</p>
+        <p class="py-2"><b>Scotch Eggs (PS, SFS, TNS) - $13.99</b> - Hard-boiled eggs wrapped in pork sausage, dusted with bread crumbs and fried. Served warm with apple beet salad and mustard sauce</p>
+        <p class="py-2"><b>Soup & Salad (ES, FS, PS, SES, SFS, TNS) - $12.99</b> - Split Pea & Ham soup served with side garden salad</p>
+        <p class="py-2"><b>Ploughman's (PS, SFS, TNS) - $23.99 (Serves 2)</b> - A feast of English cheese, crusty bread, field green salad, roasted tomatoes, cornichon pickles, apple beet salad, Branston pickle & scotch eggs (Request no scotch egg for egg-sensitive options, or request no scotch egg sauce for fish-sensitive options.)</p>
       </div>
     </div>
+  </div>
 
-    <div class="kid-items row">
-      <h2 class="fw-bold text-center">Kids' Entrées - For children 9 and under</h2>
-      <div class="col text-center">
-        <p class="fw-bold">Macaroni Cheese (ES, FS, PS, SES, SFS, SS, TNS, V) - $8.29</p>
-        <p>served with grapes and applesauce</p>
-        <p class="fw-bold">Mini Pie (ES, FS, PS, SES, SFS, TNS) - $8.29</p>
-        <p>served with grapes and applesauce</p>
-      </div>
-      <div class="col text-center">
-        <p class="fw-bold">Fish & Chips (PS, SFS, TNS) - $8.29</p>
-        <p>served with chips and tartar sauce</p>
-        <p>(Request no tartar sauce for egg-sensitive options.)</p>
-      </div>
-    </div>
+  <!-- <div class="menu-row2"></div> -->
 
-    <div class="dessert-items row">
-      <h2 class="fw-bold text-center">Desserts</h2>
-      <p class="text-center">All bakery items are made in a facility where cross-contamination with gluten, soy, peanuts, and tree nuts is possible</p>
-      <div class="col text-center">
-        <p class="fw-bold">Sticky Toffee Pudding (FS, PS, SES, SFS, SS, V) - $7.99</p>
-        <p class="fw-bold">Butterbeer™ Potted Cream (ES, FS, GS, PS, SES, SFS, SS, TNS, V) - $6.49</p>
-        <p class="fw-bold">Butterbeer™ Ice Cream (ES, FS, GS, PS, SES, SFS, SS, TNS, V) - $6.49</p>
-      </div>
-      <div class="col text-center">
-        <p class="fw-bold">Cranachan (ES, FS, PS, SES, SFS, SS, TNS, V) - $6.99</p>
-        <p class="fw-bold">Chocolate Potted Cream (FS, GS, PS, SES, SFS, SS, TNS, V) - $5.29</p>
-        <p class="fw-bold">Cup of Ice Cream (ES, FS, GS, SES, SFS, SS, TNS, V) - $6.49</p>
-        <p>Strawberry and Peanut Butter, Vanilla or Chocolate</p>
-      </div>
-    </div>
 
+<!--
     <div class="allergies d-flex px-3 pt-5">
       <p class="px-2"><span class="fw-bolder">GS</span> - Gluten Sensitive</p>
       <p class="px-2"><span class="fw-bolder">DS</span> - Dairy Sensitive</p>
@@ -115,8 +44,68 @@
       <p class="px-2"><span class="fw-bolder">SES</span> - Sesame Sensitive</p>
       <p class="px-2"><span class="fw-bolder">SS</span> - Soy Protein Sensitive</p>
     </div>
-    <p class="px-4">Menu items and pricing are subject to change without notice</p>
-  </div>
+    <p class="px-4">Menu items and pricing are subject to change without notice</p> 
+  -->
+
+<!--
+    <div class="side-items row">
+      <h2 class="fw-bold text-center">Sides</h2>
+      <div class="col text-center">
+        <p>Chips (DS, ES, FS, GS, PS, SES, SFS, SS, TNS, VG) - $4.99</p>
+        <p>Mashed Potatoes with Gravy (ES, FS, PS, SES, SFS, TNS) - $3.99</p>
+        <p>Side Garden Salad (DS, ES, FS, GS, PS, SES, SFS, SS, TNS, VG) - $6.99</p>
+      </div>
+      <div class="col text-center">
+        <p>Scotch Egg (PS, SFS, TNS) - $5.49</p>
+        <p>Add Banger (1 each) (DS, ES, FS, PS, SES, SFS, TNS) - $5.49</p>
+      </div>
+    </div>
+    <div class="sandwich-items row">
+      <h2 class="fw-bold text-center">Sandwiches</h2>
+      <p class="text-center">Served with chips</p>
+      <p class="text-center">Gluten-free buns are available upon request.</p>
+      <p></p>
+      <div class="col text-center">
+        <p>Banger (DS, FS, PS, SES, SFS, SS, TNS) - $14.99</p>
+        <p>Roasted English sausage, mustard aioli, roasted tomatoes, sautéed cabbage and onions on a crusted baguette</p>
+        <p>(Request no mustard aioli for egg-sensitive options.)</p>
+      </div>
+      <div class="col text-center">
+        <p>Specialty Chicken (FS, PS, SES, SFS, SS, TNS) - $15.99</p>
+        <p>Grilled chicken breast, apple butter mayo, Colby cheese, smoky apple bacon, roasted tomatoes on a housemade specialty bun</p>
+      </div>
+    </div>
+    <div class="kid-items row">
+      <h2 class="fw-bold text-center">Kids' Entrées - For children 9 and under</h2>
+      <div class="col text-center">
+        <p>Macaroni Cheese (ES, FS, PS, SES, SFS, SS, TNS, V) - $8.29</p>
+        <p>served with grapes and applesauce</p>
+        <p>Mini Pie (ES, FS, PS, SES, SFS, TNS) - $8.29</p>
+        <p>served with grapes and applesauce</p>
+      </div>
+      <div class="col text-center">
+        <p>Fish & Chips (PS, SFS, TNS) - $8.29</p>
+        <p>served with chips and tartar sauce</p>
+        <p>(Request no tartar sauce for egg-sensitive options.)</p>
+      </div>
+    </div>
+    <div class="dessert-items row">
+      <h2 class="fw-bold text-center">Desserts</h2>
+      <p class="text-center">All bakery items are made in a facility where cross-contamination with gluten, soy, peanuts, and tree nuts is possible</p>
+      <div class="col text-center">
+        <p>Sticky Toffee Pudding (FS, PS, SES, SFS, SS, V) - $7.99</p>
+        <p>Butterbeer™ Potted Cream (ES, FS, GS, PS, SES, SFS, SS, TNS, V) - $6.49</p>
+        <p>Butterbeer™ Ice Cream (ES, FS, GS, PS, SES, SFS, SS, TNS, V) - $6.49</p>
+      </div>
+      <div class="col text-center">
+        <p>Cranachan (ES, FS, PS, SES, SFS, SS, TNS, V) - $6.99</p>
+        <p>Chocolate Potted Cream (FS, GS, PS, SES, SFS, SS, TNS, V) - $5.29</p>
+        <p>Cup of Ice Cream (ES, FS, GS, SES, SFS, SS, TNS, V) - $6.49</p>
+        <p>Strawberry and Peanut Butter, Vanilla or Chocolate</p>
+      </div>
+    </div> 
+  -->
+
 
   <div class="">
     <router-link to="/">
@@ -152,10 +141,25 @@ export default /* class HomeView extends Vue */ {
   padding: 0.5rem;
 }
 
-.menu-items {
+.menu-row1 {
+  display: flex;
+  justify-content: space-between;
+  margin-top: -2rem;
+}
+
+.menu-row2 {
+  display: flex;
+  justify-content: space-between;
+  margin-top: -2rem;
+}
+
+.menu-items1, .menu-items2 {
+  width: 40%;
+  margin: 10rem;
   margin-top: 2rem;
+  padding: 4rem;
   background-color: #0a0a0a;
-  opacity: 0.6;
+  opacity: 0.8;
   z-index: -1;
 }
 </style>
