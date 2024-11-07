8f03c2f2bd125d40ea99a9ff891d2e35158065ac
PythonMonty <andrewnovosel90@gmail.com>
2024-11-07 06:38:00 -0500

    Breakfast Page: ONE (1) main div class (menu items)

--- src/pages/BreakfastPage.vue
+++ src/pages/BreakfastPage.vue
@@ -3,49 +3,40 @@
     Leaky Cauldron™
   </h1>
 
-  <h3 class="breakfast-title fw-bold text-center text-white mt-5">Breakfast - served until 10:30 am daily</h3>
-  <p class="mt-3 h4 text-center text-white"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
-  <p class="fw-light text-center text-white p-3">Includes small beverage</p>
+  <div class="menu-items text-white row py-5">
+    <h3 class="fw-bold text-center">Breakfast - served until 10:30 am daily</h3>
+    <p class="mt-3 h4 text-center"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
 
-  <div class="menu-row1">
-    <div class="menu-items1 text-white row py-5">
-      <div class="col text-center">
-        <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
-        <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
-        <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
-      </div>
+    <p class="fw-light text-center p-5">Includes small beverage</p>
+    
+    <div class="col text-center">
+      <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
+      <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
+      <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
     </div>
-    <div class="menu-items2 text-white row py-5">
-      <div class="col text-center">
-        <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
-        <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
-        <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
-      </div>
+
+    <div class="col text-center">
+      <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
+      <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
+      <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
     </div>
-  </div>
 
-  <div class="sides-title text-center my-4">
-    <h3 class="fw-bold text-white">Sides</h3>
-  </div>
-
-  <div class="menu-row2">
-    <div class="menu-items3 text-white row py-5">
-      <div class="sides col text-center">
+    <div class="side-items row mt-5">
+      <h3 class="fw-bold text-center">Sides</h3>
+      <div class="col text-center">
         <p class="fw-bold py-1">Bacon - $2.49</p>
         <p class="fw-bold py-1">English Bacon - $5.49</p>
         <p class="fw-bold py-1">Roasted Potatoes - $4.99</p>
       </div>
-    </div>
-    <div class="menu-items4 text-white row py-5">
-      <div class="sides col text-center">
+      <div class="col text-center">
         <p class="fw-bold py-1">Blood Sausage - $5.49</p>
         <p class="fw-bold py-1">Scrambled Eggs - $2.99</p>
       </div>
     </div>
-  </div>
 
-  <div class="ms-5 mt-3 text-white">
-    <div class="d-flex px-3">
+
+    <div class="ms-5">
+      <div class="d-flex px-3 pt-5 mt-5">
       <p class="px-2"><span class="fw-bolder">GS</span> - Gluten Sensitive</p>
       <p class="px-2"><span class="fw-bolder">DS</span> - Dairy Sensitive</p>
       <p class="px-2"><span class="fw-bolder">ES</span> - Egg Sensitive</p>
@@ -58,13 +49,11 @@
       <p class="px-2"><span class="fw-bolder">SES</span> - Sesame Sensitive</p>
       <p class="px-2"><span class="fw-bolder">SS</span> - Soy Protein Sensitive</p>
     </div>
+    </div>
+    <p class="px-4 py-3 ms-5">Menu items and pricing are subject to change without notice</p>
   </div>
 
-  <p class="px-4 py-3 ms-5 text-white">
-    Menu items and pricing are subject to change without notice
-  </p>
-
-  <div>
+  <div class="">
     <router-link to="/">
       <button>Back to home</button>
     </router-link>
@@ -77,7 +66,7 @@
 // @Options({
 //   components: {},
 // })
-export default /* class HomeView extends Vue */ {};
+export default /* class HomeView extends Vue */ {}
 </script>
 
 <style lang="scss" scoped>
@@ -92,38 +81,11 @@ export default /* class HomeView extends Vue */ {};
   padding: 0.5rem;
 }
 
-.breakfast-title {
-  margin-top: 1rem;
-}
-
-.menu-row1 {
-  display: flex;
-  justify-content: space-between;
-  margin-top: -10rem;
-}
-
-.menu-row2 {
-  display: flex;
-  justify-content: space-between;
-  margin-top: -2rem;
-}
-
-.menu-items1, .menu-items2 {
-  width: 40%;
-  margin: 10rem;
-  padding: 4rem;
+.menu-items {
+  padding: 10rem;
+  margin-top: 5rem;
   background-color: #0a0a0a;
-  opacity: 0.8;
-  z-index: -1;
-}
-
-.menu-items3, .menu-items4 {
-  width: 40%;
-  margin: 10rem;
-  margin-top: 1rem;
-  padding: 5rem;
-  background-color: #0a0a0a;
-  opacity: 0.8;
+  opacity: 0.7;
   z-index: -1;
 }
 </style>
