7b5a2cd81360a26d8c69b53213923d8a18341c8c
PythonMonty <andrewnovosel90@gmail.com>
2024-11-09 09:57:45 -0500

    Added an image in menu-items1

--- src/pages/BreakfastPage.vue
+++ src/pages/BreakfastPage.vue
@@ -1,23 +1,56 @@
 <template>
   <h1 class="restaurant text-white text-center">Leaky Cauldron™</h1>
 
-  <h3 class="fw-bold text-center text-white mt-5">Breakfast - served until 10:30 am daily</h3>
-  <p class="mt-3 h4 text-center text-white"><b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant prior to ordering if someone in your party requires special dietary accommodations</p>
+  <h3 class="fw-bold text-center text-white mt-5">
+    Breakfast - served until 10:30 am daily
+  </h3>
+  <p class="mt-3 h4 text-center text-white">
+    <b><u>Allergen Note:</u></b> Please inform a manager or Chef's Assistant
+    prior to ordering if someone in your party requires special dietary
+    accommodations
+  </p>
   <p class="fw-light text-center text-white p-3">Includes small beverage</p>
 
   <div class="menu-row1">
     <div class="menu-items1 text-white row py-5">
       <div class="col text-center">
-        <p><b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh scrambled eggs, sausage links, black pudding, English bacon, baked beans, grilled tomato, sautéed mushrooms and breakfast potatoes</p>
-        <p class="py-2"><b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three fluffy buttermilk pancakes, crisp bacon and link sausage with butter croissant</p>
-        <p class="py-2"><b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> - Pastry wrapped scrambled eggs, mushrooms and leeks served with breakfast potatoes and fresh fruit</p>
+        <p>
+          <b>Traditional Breakfast (FS, PS, SS, TNS) - $18.99</b> - Fresh
+          scrambled eggs, sausage links, black pudding, English bacon, baked
+          beans, grilled tomato, sautéed mushrooms and breakfast potatoes
+          <img :src="traditionalBreakfast" alt="Traditional Breakfast">
+        </p>
+        <p class="py-2">
+          <b>Pancake Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Three
+          fluffy buttermilk pancakes, crisp bacon and link sausage with butter
+          croissant
+        </p>
+        <p class="py-2">
+          <b>Egg, Leek, & Mushroom Pasty (FS, PS, SES, TNS) - $18.99</b> -
+          Pastry wrapped scrambled eggs, mushrooms and leeks served with
+          breakfast potatoes and fresh fruit
+        </p>
       </div>
     </div>
     <div class="menu-items2 text-white row py-5">
       <div class="col text-center">
-        <p><b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with butter croissant</p>
-        <p class="py-2"><b>Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) - $18.99</b> - Freshly baked flan of apples and oatmeal served with yogurt and fresh seasonal fruit</p>
-        <p class="py-2"><b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or American</p>
+        <p>
+          <b>American Breakfast (FS, PS, SES, SS, TNS) - $18.99</b> - Fresh
+          scrambled eggs, breakfast potatoes, crisp bacon, and link sausage with
+          butter croissant
+        </p>
+        <p class="py-2">
+          <b
+            >Apple Oatmeal Flan with Yogurt & Fruit (FS, GS, PS, SES, SS) -
+            $18.99</b
+          >
+          - Freshly baked flan of apples and oatmeal served with yogurt and
+          fresh seasonal fruit
+        </p>
+        <p class="py-2">
+          <b>Kid's Breakfast - $13.49</b> - Choose traditional, pancake or
+          American
+        </p>
       </div>
     </div>
   </div>
@@ -49,13 +82,19 @@
       <p class="px-2"><span class="fw-bolder">DS</span> - Dairy Sensitive</p>
       <p class="px-2"><span class="fw-bolder">ES</span> - Egg Sensitive</p>
       <p class="px-2"><span class="fw-bolder">PS</span> - Peanut Sensitive</p>
-      <p class="px-2"><span class="fw-bolder">TNS</span> - Tree Nut Sensitive</p>
+      <p class="px-2">
+        <span class="fw-bolder">TNS</span> - Tree Nut Sensitive
+      </p>
       <p><span class="fw-bolder">FS</span> - Fish Sensitive</p>
     </div>
     <div class="d-flex px-3">
-      <p class="px-2"><span class="fw-bolder">SFS</span> - Shellfish Sensitive</p>
+      <p class="px-2">
+        <span class="fw-bolder">SFS</span> - Shellfish Sensitive
+      </p>
       <p class="px-2"><span class="fw-bolder">SES</span> - Sesame Sensitive</p>
-      <p class="px-2"><span class="fw-bolder">SS</span> - Soy Protein Sensitive</p>
+      <p class="px-2">
+        <span class="fw-bolder">SS</span> - Soy Protein Sensitive
+      </p>
     </div>
   </div>
 
@@ -76,7 +115,13 @@
 // @Options({
 //   components: {},
 // })
-export default /* class HomeView extends Vue */ {};
+export default /* class HomeView extends Vue */ {
+  data() {
+    return {
+      traditionalBreakfast: require("@/assets/images/traditional-breakfast.png"),
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -103,7 +148,8 @@ export default /* class HomeView extends Vue */ {};
   margin-top: -2rem;
 }
 
-.menu-items1, .menu-items2 {
+.menu-items1,
+.menu-items2 {
   width: 40%;
   margin: 10rem;
   padding: 4rem;
@@ -112,7 +158,8 @@ export default /* class HomeView extends Vue */ {};
   z-index: -1;
 }
 
-.menu-items3, .menu-items4 {
+.menu-items3,
+.menu-items4 {
   width: 100%;
   margin: 10rem;
   margin-top: 1rem;
